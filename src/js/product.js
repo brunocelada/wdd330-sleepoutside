ecdd96a7773f9ce51d5e12d83516a3a55ed682ae
brunocelada <brunocelada-4@hotmail.com>
2026-09-08 11:06:52 -0300

    W02 final solution Team Activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,34 +1,41 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
-console.log(dataSource.findProductById(productId));
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-function addProductToCart(product) {
-  // 1. Get current cart items from localStorage, or default to an empty array
-  let cartItems = getLocalStorage("so-cart") || [];
+// LOG TO SEE THE OBJECT RETURNED FROM THE DATASOURCE.
+// console.log(dataSource.findProductById(productId));
 
-  // 2. Ensure it's treated as an array
-  if (!Array.isArray(cartItems)) {
-    cartItems = [cartItems];
-  }
 
-  // 3. Push the new product into the array
-  cartItems.push(product);
+// NO NEEDED ANY MORE:
+// function addProductToCart(product) {
+//   // 1. Get current cart items from localStorage, or default to an empty array
+//   let cartItems = getLocalStorage("so-cart") || [];
 
-  // 4. Save the updated array back to localStorage
-  setLocalStorage("so-cart", cartItems);
-}
+//   // 2. Ensure it's treated as an array
+//   if (!Array.isArray(cartItems)) {
+//     cartItems = [cartItems];
+//   }
 
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+//   // 3. Push the new product into the array
+//   cartItems.push(product);
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+//   // 4. Save the updated array back to localStorage
+//   setLocalStorage("so-cart", cartItems);
+// }
+
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);

3cdf2e77cd18d18a7ec069fa0f5785d1cfe1b9d8
brunocelada <brunocelada-4@hotmail.com>
2026-09-11 11:59:33 -0300

    remove the first instance of the item.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,19 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+  const listElement = document.querySelector(".product-list");
+
+  // BC- Check if the cart is empty and display the warning.
+  if (cartItems.length === 0) {
+    listElement.innerHTML = "<p>The cart is empty</p>";
+    return;
+  };
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  listElement.innerHTML = htmlItems.join("");
+
+  addRemoveListeners();
 }
 
 function cartItemTemplate(item) {
@@ -20,9 +30,34 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="remove-item" data-id="${item.Id}">Remove &#10006;</span>
 </li>`;
+  // Added span X remover item.
 
   return newItem;
+}
+
+//finding Xs in cart, identifying item clicked, remove from local storage, cart refresh (does not remove duplicates one at a time) -KD
+// BC- When an item is removed, all items with the same ID are deleted. I fixed that error. Now, remove the first instance of the item.
+function addRemoveListeners() {
+  const removeItems = document.querySelectorAll(".remove-item");
+
+  removeItems.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      const id = event.target.dataset.id;
+
+      const cartItems = getLocalStorage("so-cart");
+
+      // BC- here are my changes.
+      const index = cartItems.findIndex((cartItem) => cartItem.Id === id);
+      if (index !== -1) {
+        cartItems.splice(index, 1);
+      };
+
+      setLocalStorage("so-cart", cartItems);
+      renderCartContents();
+    });
+  });
 }
 
 renderCartContents();

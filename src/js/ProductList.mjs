d2646783de6b8743e8b48d8f225d34d9bc8c3cda
jensen-westraad <jensenwestraad@gmail.com>
2026-09-12 20:24:59 +0200

    Added discount indicators for product listings

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,10 +1,25 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
+    const discount =
+        product.FinalPrice < product.SuggestedRetailPrice
+            ? Math.round(
+                ((product.SuggestedRetailPrice - product.FinalPrice) /
+                    product.SuggestedRetailPrice) *
+                100,
+            )
+            : 0;
+
+    const discountIndicator =
+        discount > 0
+            ? `<span class="discount-indicator">SALE - ${discount}% OFF</span>`
+            : "";
+
     return `<li class="product-card">
     <a href="product_pages/?product=${product.Id}">
       <img src="${product.Image}" alt="Image of ${product.Name}">
-      <h2 class="card__brand">${product.Brand}</h2>
+      ${discountIndicator}
+      <h2 class="card__brand">${product.Brand.Name}</h2>
       <h3 class="card__name">${product.NameWithoutBrand}</h3>
       <p class="product-card__price">$${product.FinalPrice}</p>
     </a>

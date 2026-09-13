5c678d463664ea6e6bff1a9b56e5793d871a2a04
Bruno Celada <121876956+brunocelada@users.noreply.github.com>
2026-09-12 22:59:48 -0300

    Revert "W02 Dynamic Product List - Irene"

--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import ProductData from "./ProductData.mjs";
-
-const dataSource = new ProductData("tents");
-
-import ProductList from "./ProductList.mjs";
-
-const element = document.querySelector(".product-list");
-const listing = new ProductList("tents", dataSource, element);
-listing.init();

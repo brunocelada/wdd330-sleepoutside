e9bb61af2a3d64f638fb1435a1ba3ba4ac98d6ba
Irene Gavina Costales <>
2026-09-16 01:12:27 +0800

    Add alert container and initialize alert component

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,12 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { updateCartCount } from "./cartCount.mjs";
 
+import Alert from "./Alert.js";
+
+// Load alerts when the page loads
+const alert = new Alert();
+alert.init();
+
 const dataSource = new ProductData("tents");
 
 const element = document.querySelector(".product-list");
